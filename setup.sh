bf6843fbb2910d2c255e718d1166a1aaefd68a79
Claude Pageau <pageauc@gmail.com>
2018-05-14 07:12:27 -0400

    release 0.96 fix python-sonic install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Convenient sonic-track-install.sh script written by Claude Pageau 1-Jul-2016
-ver="0.95"
+ver="0.96"
 DEST_DIR='sonic-track'  # Default folder install location
 
 cd ~
@@ -45,10 +45,10 @@ if [ $? -ne 0 ] ;  then
   wget -O config.py.notes-drums https://raw.github.com/pageauc/sonic-track/master/config.py.notes-drums
   wget -O sonic-track.sh https://raw.github.com/pageauc/sonic-track/master/sonic-track.sh  
   wget -O sonic-track.py https://raw.github.com/pageauc/sonic-track/master/sonic-track.py
-  wget -O setup.sh https://raw.github.com/pageauc/sonic-track/master/setup.sh 
+#  wget -O setup.sh https://raw.github.com/pageauc/sonic-track/master/setup.sh 
   wget -O cv32-setup.sh https://raw.github.com/pageauc/opencv3-setup/master/cv32-setup.sh  
   wget -O Readme.md https://raw.github.com/pageauc/sonic-track/master/Readme.md
-  wget -O psonic.py https://raw.github.com/gkvoelkl/python-sonic/master/psonic.py
+  # wget -O psonic.py https://raw.github.com/gkvoelkl/python-sonic/master/psonic.py
 else
   wget -O config.py -q --show-progress https://raw.github.com/pageauc/sonic-track/master/config.py
   wget -O config.py.default -q --show-progress https://raw.github.com/pageauc/sonic-track/master/config.py.default
@@ -57,10 +57,10 @@ else
   wget -O config.py.notes-drums -q --show-progress https://raw.github.com/pageauc/sonic-track/master/config.py.notes-drums
   wget -O sonic-track.sh -q --show-progress https://raw.github.com/pageauc/sonic-track/master/sonic-track.sh
   wget -O sonic-track.py -q --show-progress https://raw.github.com/pageauc/sonic-track/master/sonic-track.py
-  wget -O setup.sh -q --show-progress https://raw.github.com/pageauc/sonic-track/master/setup.sh 
+#  wget -O setup.sh -q --show-progress https://raw.github.com/pageauc/sonic-track/master/setup.sh 
   wget -O cv32-setup.sh -q --show-progress https://raw.github.com/pageauc/opencv3-setup/master/cv32-setup.sh  
   wget -O Readme.md -q --show-progress https://raw.github.com/pageauc/sonic-track/master/Readme.md
-  wget -O psonic.py -q --show-progress https://raw.github.com/gkvoelkl/python-sonic/master/psonic.py
+  # wget -O psonic.py -q --show-progress https://raw.github.com/gkvoelkl/python-sonic/master/psonic.py
 fi
   
 echo "Done Download"
@@ -69,7 +69,7 @@ echo "2 - Make Required Files Executable"
 echo ""
 chmod +x *py
 chmod -x config*py
-chmod -x psonic*py
+# chmod -x psonic*py
 chmod +x *sh
 echo "Done Permissions"
 echo "-------------------------------------------------------------"
@@ -96,8 +96,17 @@ echo "------------------------------------------------"
 echo ""  
 echo "5 - Installing sonic-track Dependencies"
 echo ""
-sudo apt-get install -yq python-picamera python-pip dos2unix
-sudo pip3 install python-osc
+sudo apt-get install -yq python-picamera
+sudo apt-get install -yq python3-picamera
+sudo apt-get install -yq python-pip
+sudo apt-get install -yq git
+sudo apt-get install -yq dos2unix
+sudo pip install python-osc
+git clone https://github.com/gkvoelkl/python-sonic
+cd python-sonic
+sudo python3 setup.py install
+cd ..
+rm -r python-sonic
 
 cd $DIR
 # Check if install.sh was launched from sonic-track folder
@@ -110,6 +119,18 @@ fi
 
 echo "Done Dependencies"
 
+echo "Make sure you have
+1 opencv3 installed see https://github.com/pageauc/opencv3-setup
+2 speaker connected or RPI on HDTV via HTMI cable
+3 picamera or webcam installed and working
+4 config.py edited as required eg webcam setting if needed
+5 For Details See https://github.com/pageauc/sonic-track
+
+To launch
+    cd ~/sonic-track
+    ./sonic-track.sh
+
+"
 echo $DEST_DIR "Good Luck Claude ..."
 echo "Bye"
 
